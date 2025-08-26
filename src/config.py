be30df5933b83417bb49152582476afcb4e227a1
Addy06 <aadhityaayyappan06@gmail.com>
2025-08-26 22:42:55 +0530

    keyword config

---
+++ src/config.py
@@ -0,0 +1,15 @@
+
+STOCK_KEYWORDS = [
+    "NIFTY50", "NIFTY", "Sensex", "BSE", "NSE",
+    "Reliance", "TCS", "Infosys", "HDFC Bank", "ICICI Bank",
+    "SBI", "Adani", "Wipro", "HCLTech", "Asian Paints",
+    "Kotak Mahindra", "Bajaj Finance", "LTIMindtree", "ITC"
+]
+
+REDDIT_SUBREDDITS = [
+    "IndianStockMarket", "stocks", "investing", "finance",
+    "StockMarketIndia", "IndianEconomy"
+]
+
+
+TWITTER_LANG = "en"
